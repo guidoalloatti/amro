e64f6bd81588746b3fb032ad548a7f2e9dc07355
Guido Alloatti <guidoalloatti@gmail.com>
2011-03-28 18:31:22 -0300

    guido-commit-2. Adding Main.php

---
+++ AmroClient/main.php
@@ -0,0 +1,25 @@
+<?php
+
+/* invoice_url options are:
+ *    * certificado (Certificados)
+ *    * index (Menu Principal - Default)
+ *    * oc (Ordenes de Compra)
+ */
+
+ echo "ACA: ".$_GET["invoice_url"];
+ 
+if(!isset($_GET["invoice_url"]))
+{
+	$invoice_url = "index";
+}
+else
+{
+	$invoice_url = $_GET["invoice_url"]; 
+}
+	
+require_once("include/render.php");
+$myRender = new Render($invoice_url);
+$myRender->getPage();
+$myRender->renderPage();
+
+?>
